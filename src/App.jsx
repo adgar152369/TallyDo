12b9f3fdc8ba39ac4cbc492ca9bf13e2929ae71f
Adam Garcia <gar15041@byui.edu>
2023-05-28 21:19:19 -0600

    enter key saves edited input cont...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './components/Container/Container';
 import UserInput from './components/Input/UserInput/UserInput';
 import CreateList from './components/Lists/CreateList/CreateList';
@@ -13,6 +13,25 @@ function App() {
   const [itemInput, setItemInput] = useState('');
   const [editedItemInput, setEditedItemInput] = useState('');
   const [editedItemIndex, setEditedItemIndex] = useState(-1);
+  const [isEditing, setIsEditing] = useState(false);
+
+  console.log(quickList)
+
+  useEffect(() => {
+    console.log(isEditing);
+    if (isEditing) {
+      const handleKeyDown = (event) => {
+        if (event.key === 'Enter' || event.key === 'Escape') {
+          setIsEditing(false);
+        }
+      };
+      document.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+      };
+    }
+  }, [isEditing]);
 
   function handleAddToQuickList(newQuickItem) {
     setQuickList([...quickList, newQuickItem]);
@@ -29,6 +48,7 @@ function App() {
   function handleEditedItem(e, index, editedItem) {
     e.preventDefault();
     setEditedItemInput(editedItem);
+    console.log(editedItem);
 
     for (let i = 0; i < quickList.length; i++) {
       if (i === index) {
@@ -41,19 +61,21 @@ function App() {
     <Container>
       <div className="input-container custom-lg:flex">
         <img className='cat-image' src={cuteCat} />
-        <UserInput 
-          onSetQuickList={handleAddToQuickList} userItemInput={itemInput} 
+        <UserInput
+          onSetQuickList={handleAddToQuickList} userItemInput={itemInput}
           onSetItemInput={setItemInput}
           onHandleItemInputChange={handleItemInputChange} />
       </div>
 
-      <QuickList 
-        list={quickList} 
-        onDeleteItem={deleteFromQuickList} 
+      <QuickList
+        list={quickList}
+        onDeleteItem={deleteFromQuickList}
         onHandleItemInputChange={handleItemInputChange}
         onHandleEditedItem={handleEditedItem}
         userEditedItem={editedItemInput}
-        editedItemIndex={editedItemIndex}/>
+        editedItemIndex={editedItemIndex}
+        isEditing={isEditing}
+        onSetIsEditing={setIsEditing} />
     </Container>
   )
 }
--- src/components/Lists/QuickList/QuickList.jsx
+++ src/components/Lists/QuickList/QuickList.jsx
@@ -4,8 +4,8 @@ import EditInput from "../../Input/UserInput/EditInput/EditInput";
 
 function QuickList(props) {
 
-  const [editing, setEditing] = useState(false);
-  
+  // const [editing, setEditing] = useState(false);
+
   let holdTimeout;
   let holdDuration = 600; // duration in ms
 
@@ -16,9 +16,9 @@ function QuickList(props) {
   function handleHoldEdit(e) {
     if (e.button === 0) {
       clearTimeout(holdTimeout); // clear any existing timeout
-      
+
       holdTimeout = setTimeout(() => { // start new timeout
-        setEditing(true);
+        props.onSetIsEditing(true);
       }, holdDuration);
     }
   }
@@ -39,27 +39,25 @@ function QuickList(props) {
             props.list ?
               props.list.map((item, index) => (
 
-                <li 
-                  className={`${styles.listItem} ${editing && styles.editingListItem}`} 
-                  key={index}>
+                <li
+                  className={`${styles.listItem} ${props.isEditing && styles.editingListItem}`}
+                  key={index}
+                  onDoubleClick={() => deleteItem(index)}
+                  onMouseDown={handleHoldEdit}
+                  onTouchStart={handleHoldEdit}
+                  onMouseUp={handleHoldEnd}
+                  onTouchEnd={handleHoldEnd}>
 
-                  <span 
-                    className={`${styles.listItemSpan} ${editing && styles.viewMode}`}
-                    onDoubleClick={() => deleteItem(index)}
-                    onMouseDown={handleHoldEdit}
-                    onTouchStart={handleHoldEdit} 
-                    onMouseUp={handleHoldEnd}
-                    onTouchEnd={handleHoldEnd}>
-                    
+                  <span className={`${styles.listItemSpan} ${props.isEditing && styles.viewMode}`}>
                     {props.editedItemIndex === index ? props.userEditedItem : item}
                   </span>
 
-                  <EditInput 
+                  <EditInput
                     onHandleEditedItem={props.onHandleEditedItem}
                     key={index}
                     itemIndex={index}
                     editMode={styles.editMode}
-                    isEditing={editing}
+                    isEditing={props.isEditing}
                     defaultValue={item}
                   />
 
